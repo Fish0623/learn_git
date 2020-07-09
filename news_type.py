856bdc40b10b7e8a910b76d15d8fc03c33c546a6
yuyu <845290986@qq.com>
2020-07-10 00:37:59 +0800

    1.0

--- news_type.py
+++ news_type.py
@@ -4,6 +4,6 @@ from HttpClientPyLibs.bussiness import bussiness_common
 from AT_JSON.at_json import json_get_json_list_value
 from HttpClientPyLibs.http_base.logger_handler import http_client_log
 a = pre_ecfront.PreNewsGetList()
-# list = []
+# list = [112]
 list = a.news_get_list(page=100, everypage_count=10,class_id=1)
 print(list)
